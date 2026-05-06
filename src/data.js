9a7685b7a516f3dfdbd029a5ecd65e263a50d96d
Vinícius Diller <viniciusdiller@hotmail.com>
2026-05-06 12:32:06 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const projetosMock = [
+  {
+    id: 1,
+    titulo: "PROJETO BLOCKBUSTER",
+    categoria: "Residencial",
+    imagem:
+      "https://images.unsplash.com/photo-1600210492486-724fe5c67fb0?q=80&w=2000&auto=format&fit=crop",
+  },
+  {
+    id: 2,
+    titulo: "PROJETO SINGAPURA",
+    categoria: "Comercial",
+    imagem:
+      "https://images.unsplash.com/photo-1600607687920-4e2a09cf159d?q=80&w=2000&auto=format&fit=crop",
+  },
+  {
+    id: 3,
+    titulo: "CASA COR RIO 2024",
+    categoria: "Mostra",
+    imagem:
+      "https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?q=80&w=2000&auto=format&fit=crop",
+  },
+  {
+    id: 4,
+    titulo: "PROJETO ALEMANHA",
+    categoria: "Residencial",
+    imagem:
+      "https://images.unsplash.com/photo-1600566753190-17f0baa2a6c3?q=80&w=2000&auto=format&fit=crop",
+  },
+];
